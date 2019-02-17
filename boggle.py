9e8bed54050be1d58d2ddd0e41c49bbedaa33f5e
Keith Scully <keithscully83@gmail.com>
2019-02-17 19:53:07 +0000

    change words dictionary returned from get_dictionary function from a list to a set to improve algorithm speed

--- boggle.py
+++ boggle.py
@@ -84,7 +84,7 @@ def get_dictionary(dictionary_file):
     Load dictionary file
     """
     with open(dictionary_file) as f:
-        return [w.strip().upper() for w in f]
+        return {w.strip().upper() for w in f}
 
 def main():
     """
